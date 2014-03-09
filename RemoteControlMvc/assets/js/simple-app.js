5040b2902451c725b6f244c5731c4b0ae372b14b
Fazzani <tunisienheni@outlook.com>
2014-03-09 13:46:10 +0100

    Hfa
    Signed-off-by: Fazzani <tunisienheni@outlook.com>

--- RemoteControlMvc/assets/js/simple-app.js
+++ RemoteControlMvc/assets/js/simple-app.js
@@ -8,7 +8,7 @@ $(function() {
 	$.mobile.pushStateEnabled = false;
 	$.event.special.swipe.scrollSupressionThreshold =10;// More than this horizontal displacement, and we will suppress scrolling.
 	$.event.special.swipe.horizontalDistanceThreshold =30;// Swipe horizontal displacement must be more than this.
-	$.event.special.swipe.durationThreshold =500;// More time than this, and it isn't a swipe.
+	$.event.special.swipe.durationThreshold = 500;// More time than this, and it isn't a swipe.
 	$.event.special.swipe.verticalDistanceThreshold =75;
 	//$.mobile.ajaxEnabled = false;
     // This second step ensures that the insertion of the new toolbar does not  affect page height
